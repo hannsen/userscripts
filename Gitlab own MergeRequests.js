fdf8a2e85401dd6308746bde9345c03f480d9e4b
hannsen <jan@hro-home.de>
2018-01-17 19:50:15 +0100

    Update Gitlab own MergeRequests.js

--- Gitlab own MergeRequests.js
+++ Gitlab own MergeRequests.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         My Merge Requests gitlab
+// @name         My Merge Requests Gitlab
 // @namespace    http://tampermonkey.net/
-// @version      0.9
-// @description  My Merge Requests link in gitlab
-// @author       You
+// @version      1.0
+// @description  Show Link to opened Merge Requests
+// @author       hannsen
 // @match        https://git04.quodata.de/*
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @downloadURL  https://raw.githubusercontent.com/hannsen/userscripts/master/Gitlab%20own%20MergeRequests.js
+// @updateURL    https://raw.githubusercontent.com/hannsen/userscripts/master/Gitlab%20own%20MergeRequests.js
 // @grant        none
 // ==/UserScript==
 
@@ -15,8 +17,8 @@
     var $merge_button = $($(".user-counter:eq( 1 )").prop('outerHTML'));
     var new_href = $merge_button.children().attr('href').replace('assignee_id','state=opened&author_id');
     $merge_button.children().attr('href',new_href);
-    $merge_button.find('span').toggleClass('hidden gitlab-own-merge-requests merge-requests-count issues-count');
-
+    $merge_button.find('span').toggleClass('gitlab-own-merge-requests merge-requests-count issues-count');
+    $merge_button.find('span').removeClass('hidden');
     $($merge_button.prop('outerHTML')).insertBefore( ".user-counter:eq( 2 )" );
 
     $.ajax({
