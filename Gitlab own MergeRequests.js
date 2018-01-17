439d5b50ad1e1cf067d3cc735f347f75f61ef1a2
hannsen <jan@hro-home.de>
2018-01-17 14:59:02 +0100

    Update Gitlab own MergeRequests.js

--- Gitlab own MergeRequests.js
+++ Gitlab own MergeRequests.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Merge Requests gitlab
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  My Merge Requests link in gitlab
 // @author       You
 // @match        https://git04.quodata.de/*
@@ -15,7 +15,7 @@
     var $merge_button = $($(".user-counter:eq( 1 )").prop('outerHTML'));
     var new_href = $merge_button.children().attr('href').replace('assignee_id','state=opened&author_id');
     $merge_button.children().attr('href',new_href);
-    $merge_button.find('span').html('M').toggleClass('hidden gitlab-own-merge-requests');
+    $merge_button.find('span').toggleClass('hidden gitlab-own-merge-requests merge-requests-count issues-count');
 
     $($merge_button.prop('outerHTML')).insertBefore( ".user-counter:eq( 2 )" );
 
@@ -24,7 +24,7 @@
     })
         .done(function( data ) {
         var open_mr = $(data).find('a#state-opened > span.badge').html();
-        $('.gitlab-own-merge-requests').html('M' + open_mr );
+        $('.gitlab-own-merge-requests').html('' + open_mr );
     });
 
 })();
