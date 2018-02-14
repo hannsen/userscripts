8c54c189b88f854a7b047b970dc2181b4d886c82
hannsen <jan@hro-home.de>
2018-02-14 11:56:53 +0100

    formatting

--- GitLab estimated time extractor.js
+++ GitLab estimated time extractor.js
@@ -7,6 +7,8 @@
 // @match        *://git04.quodata.de/*/*/issues*
 // @match        *://git04.quodata.de/dashboard/issues*
 // @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @downloadURL  https://raw.githubusercontent.com/hannsen/userscripts/master/GitLab%20estimated%20time%20extractor.js
+// @updateURL    https://raw.githubusercontent.com/hannsen/userscripts/master/GitLab%20estimated%20time%20extractor.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,18 +20,17 @@
     var pattIssue = /.*\/issues\/\d*$/i;
     $('.issues-list').find('a').each(function(){
         if($(this).attr('href').match(pattIssue)){
-          $.ajax({
+            $.ajax({
                 url: $(this).attr('href'),
             }).done(function( data ) {
-              var $data_json = $(data).find('div[data-noteable-data]').attr('data-noteable-data');
-              var $json = JSON.parse($data_json);
-              var time_estimate =  $json.human_time_estimate;
-              if (time_estimate != null){
-                  $('#issue_' + $json.id).find('.issuable-info').append('<span class="label color-label" style="background-color: #222222">' + time_estimate + '</span>');
-              }
+                var $data_json = $(data).find('div[data-noteable-data]').attr('data-noteable-data');
+                var $json = JSON.parse($data_json);
+                var time_estimate =  $json.human_time_estimate;
+                if (time_estimate != null){
+                    $('#issue_' + $json.id).find('.issuable-info').append(
+                        '<span class="label color-label" style="background-color: #222222">' + time_estimate + '</span>');
+                }
             });
         }
     });
-
-
 })();
