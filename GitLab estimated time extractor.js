46a2763275a59819ff656a530cd9ac068c8e910d
hannsen <jan@hro-home.de>
2018-05-18 14:45:10 +0200

    Update GitLab estimated time extractor.js

--- GitLab estimated time extractor.js
+++ GitLab estimated time extractor.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         GitLab estimated time extractor
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Extract time estimations from gitlab issues to the issue list
+// @version      1.3
+// @description  Extract time estimations from gitlab issues to the issue list, also copyable to excel
 // @author       hannsen
 // @match        *://git04.quodata.de/*/*/issues*
 // @match        *://git04.quodata.de/dashboard/issues*
@@ -13,9 +13,11 @@
 // ==/UserScript==
 
 
+
 (function() {
     'use strict';
 
+    var toExportes = "Name\tAssigned\tMilestone\tZeitschaetzung\n";
     var hrefs = [];
     var pattIssue = /.*\/issues\/\d*$/i;
     $('.issues-list').find('a').each(function(){
@@ -26,11 +28,43 @@
                 var $data_json = $(data).find('div[data-noteable-data]').attr('data-noteable-data');
                 var $json = JSON.parse($data_json);
                 var time_estimate =  $json.human_time_estimate;
+
+                var toExport = "";
+                // title
+                toExport += $json.title  + "\t";
+                // assignee
+                var assignee_name = $json.assignees[0] ? $json.assignees[0].name : "";
+                toExport += assignee_name + "\t";
+                // milestone name
+                var milestone = $json.milestone ? $json.milestone.title : "";
+                toExport += milestone + "\t";
+                //time estimate
+                toExport += time_estimate || "" + "\t";
                 if (time_estimate != null){
                     $('#issue_' + $json.id).find('.issuable-info').append(
                         '<span class="label color-label" style="background-color: #222222">' + time_estimate + '</span>');
                 }
+                toExport += "\n";
+                toExportes += toExport;
             });
         }
     });
+
+    var $swipeButton = $('.btn.btn-default.has-tooltip').clone();
+    $swipeButton.removeAttr('href').click( function(){
+        copyToClipboard(toExportes); alert('Copied to clipboard, paste to excel'); });
+    $swipeButton.text('Copy issue info');
+    $swipeButton.insertAfter('.btn.btn-default.has-tooltip');
+
+    var copyToClipboard = str => {
+        const el = document.createElement('textarea');
+        el.value = str;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    };
+
+
 })();
+
