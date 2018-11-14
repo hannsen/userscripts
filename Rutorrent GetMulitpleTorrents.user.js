ff94311e3f547ca1c6202cf8d783aacdac8bad48
hannsen <jan.karberg@quodata.de>
2018-11-14 09:34:38 +0100

    open all selected elements, if they match regex

--- Rutorrent GetMulitpleTorrents.user.js
+++ Rutorrent GetMulitpleTorrents.user.js
@@ -5,7 +5,7 @@
 // @description gets multiple torrents which you select in rutorrent
 // @include     http://*ruTorrent*
 // @include     https://*ruTorrent*
-// @version     2.9
+// @version     3.0
 // @grant       none
 // @downloadURL  https://github.com/hannsen/userscripts/raw/master/Rutorrent%20GetMulitpleTorrents.user.js
 // @updateURL    https://github.com/hannsen/userscripts/raw/master/Rutorrent%20GetMulitpleTorrents.user.js
@@ -14,27 +14,30 @@
 var nav_bar = document.getElementById("t");
 
 var unfoldLink = document.createElement('a');
-var newHTML = document.createElement ('div');
+var newHTML = document.createElement('div');
 newHTML.id = 'moved';
 unfoldLink.appendChild(newHTML);
 unfoldLink.id = 'grab';
 unfoldLink.title = 'Download selected torrent files';
 unfoldLink.href = 'javascript:void(0)';
 
-if(nav_bar){
+if (nav_bar) {
   nav_bar.appendChild(unfoldLink);
   document.getElementById('grab').addEventListener('click', getTorrents, false);
 }
 
-function getTorrents(){
+function getTorrents() {
   var selected = document.getElementsByClassName("selected");
   var dl_link = 'plugins/source/action.php?hash=';
 
-  if('/' != window.location.toString().slice(-1)){
+  if ('/' != window.location.toString().slice(-1)) {
     dl_link = '/' + dl_link;
   }
 
-  for (var i=0;i<(selected.length-3);i++){
-    window.open(window.location + dl_link + selected[i].id);
+  for (var i = 0; i < (selected.length - 1); i++) {
+    var hash = selected[i].id;
+    if (hash && hash.match(/[A-F0-9]{40}/i)) {
+      window.open(window.location + dl_link + hash);
+    }
   }
 }
