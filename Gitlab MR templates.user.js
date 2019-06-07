713c9857c3c4986b01032f1c49361caa8f497a2c
hannsen <jan@hro-home.de>
2019-06-07 10:53:42 +0200

    Update Gitlab MR templates.user.js

--- Gitlab MR templates.user.js
+++ Gitlab MR templates.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         MR Templates Gitlab
 // @namespace    http://quodata.de/
-// @version      1.0
+// @version      1.1
 // @description  Show merge request templates
 // @author       hannsen
-// @match        https://git04.quodata.de/it/delphi/merge_requests/new+
-// @match        https://git04.quodata.de/it/oequasta/merge_requests/new+
-// @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/merge_requests/new+
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/merge_requests/new+
-// @match        https://git04.quodata.de/qd_drupal_projects/hub/merge_requests/new+
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_base_entity/merge_requests/new+
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_screenshottests/merge_requests/new+
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_datatables_form/merge_requests/new+
+// @match        https://git04.quodata.de/it/delphi/merge_requests/new*
+// @match        https://git04.quodata.de/it/oequasta/merge_requests/new*
+// @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/hub/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/qd_base_entity/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/qd_screenshottests/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/qd_datatables_form/merge_requests/new*
 // ==/UserScript==
 (function() {
     'use strict';
