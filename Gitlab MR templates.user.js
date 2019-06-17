5c6afa4ace46abbb634ee3106ef85df1a776dbff
hannsen <jan@hro-home.de>
2019-06-17 10:13:22 +0200

    update url

--- Gitlab MR templates.user.js
+++ Gitlab MR templates.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MR Templates Gitlab
 // @namespace    http://quodata.de/
-// @version      1.1
+// @version      1.2
 // @description  Show merge request templates
 // @author       hannsen
 // @match        https://git04.quodata.de/it/delphi/merge_requests/new*
@@ -12,6 +12,8 @@
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_base_entity/merge_requests/new*
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_screenshottests/merge_requests/new*
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_datatables_form/merge_requests/new*
+// @downloadURL  https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
+// @updateURL    https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
 // ==/UserScript==
 (function() {
     'use strict';
