707694bab7fe4424712df61a7a6c02b098d77e6c
hannsen <jan.karberg@quodata.de>
2019-08-23 09:12:10 +0200

    Create gitlab_changelog_getter.user.js

---
+++ gitlab_changelog_getter.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name         Gitlab Changelog Getter
+// @namespace    http://git04.quodata.de
+// @version      0.1
+// @description  Gitlab Changelog Getter
+// @author       hoanns
+// @match        https://git04.quodata.de/*/merge_requests
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+
+    var $ = jQuery;
+
+    var project = window.location.href.match(/quodata.de\/(.+?)\/merge/)[1];
+    var ids = {
+        "qd_drupal_projects/hub" : "154",
+        "qd_drupal_projects/qd_ptop_modules_project": "155",
+        "it/oequasta-kollektivbildung": "96",
+        "it/oequasta": "85",
+    };
+    var project_id = ids[project];
+
+    var $butt = $('<button class="btn">Get Changelog</button>');
+    function addGUI(){
+        $('div.nav-controls').prepend($butt);
+        $('div.nav-controls').prepend('<input type="date" name="changelog" style="width:140px">');
+    }
+    if(project_id){
+        addGUI();
+    }
+
+
+    $butt.click(function() {
+        var datum = $('input[name="changelog"]').val();
+        if (!datum) {
+            alert('Choose date');
+            return;
+        }
+
+        $.getJSON(
+            'https://git04.quodata.de/api/v4/projects/'+ project_id +'/merge_requests?state=merged&updated_after=' + datum + '&per_page=100',
+            function(data) {
+                var changelog = filterChangelog(data, datum);
+                $('html').html('<pre>' + changelog + '</pre>');
+            });
+
+    });
+
+    function filterChangelog(data, datum){
+        var changelog = '';
+        $(data).each(function(){
+            if(!this.merged_at >= datum){
+                return;
+            }
+            var desc = this.description;
+            // [\s\S] = dotall modifier hack
+            var match = desc.match(/CHANGELOG entry([\s\S]+?)###/);
+            if(match && match.length >= 2){
+                var hit = match[1];
+                hit = hit.replace('<!-- Falls es Kunden etwas angeht; in Kundensprache. -->', '');
+
+                changelog += hit;
+            }
+        });
+        return changelog;
+    }
+
+})();
