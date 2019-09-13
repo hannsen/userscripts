1a58d44c2b8cd05d0aee39eb4b87b59de15b8c4e
hannsen <jan.karberg@quodata.de>
2019-09-13 14:09:10 +0200

    Update Gitlab own MergeRequests.user.js

--- Gitlab own MergeRequests.user.js
+++ Gitlab own MergeRequests.user.js
@@ -2,7 +2,7 @@
 // https://github.com/hannsen/userscripts
 // @name         My Merge Requests Gitlab
 // @namespace    http://tampermonkey.net/
-// @version      2.6
+// @version      2.7
 // @description  Show Link to opened Merge Requests, auto click swipe on MR with pics
 // @author       hannsen
 // @match        https://git04.quodata.de/*
@@ -14,6 +14,7 @@
 (function() {
     'use strict';
 
+    // Old function to try to select a certain picture comparing method in merge requests
     //    var swipe = 0;
     //    function scrollFunction() {
     //        if(!swipe)
@@ -38,25 +39,45 @@
     //
     //
 
-    // Set ur own clone url prefix with GM_setValue("git_url_prefix", "username:token");
     var git_url_prefix = GM_getValue("git_url_prefix");
     var $http_clone = $('input[name="http_project_clone"]');
+
+
+    // Add gui for setting prefix
+    if($http_clone.length){
+        var titletext = "Prefix to add to the clone url across all projects";
+        var butt = $('<input type="button" value="Set prefix" title="' + titletext +'" />');
+        var input = $('<input id="clone_url_token" placeholder="username:token" title="' + titletext + '" />');
+        var line = $http_clone.closest('li.pt-2');
+        line.append('<br>', input, butt);
+
+        butt.click(function(){
+            var val = $('input#clone_url_token').val();
+            if(val)
+                GM_setValue("git_url_prefix", val);
+        });
+    }
+
+
+    // Adding prefix to clone url
     if(git_url_prefix && $http_clone.val()){
         var clone_url = $http_clone.val();
         clone_url = clone_url.replace("https://", "https://" + git_url_prefix + "@");
         $http_clone.val(clone_url);
     }
 
+
+    // Coloring collapsed diffs
     function colorCollapsed(){
         $("div.diff-collapsed").css('background-color', 'red');
     }
-
     if(window.location.href.indexOf("merge_requests") > 0){
         window.onscroll = colorCollapsed;
     }
 
-    var last_mr_count = GM_getValue("open_mr_count") || 0;
 
+    // Adding own merge request button
+    var last_mr_count = GM_getValue("open_mr_count") || 0;
     var $merge_button = $($(".user-counter:eq( 1 )").prop('outerHTML'));
     var new_href = $merge_button.children().attr('href').replace('assignee_username', 'scope=all&state=opened&author_username');
     $merge_button.children().attr('href', new_href);
@@ -73,6 +94,7 @@
             $('.gitlab-own-merge-requests').html(open_mr_count);
             GM_setValue("open_mr_count", open_mr_count);
         });
+
 
     // Grey out issues with pending Merge Request
     if(document.location.href.indexOf('dashboard/issues?') !== -1 && document.location.href.indexOf('assignee_username=') !== -1){
