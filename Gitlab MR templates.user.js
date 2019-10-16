86528a74cd2c29a875b8c30482878c1ba0a43251
hannsen <jan@hro-home.de>
2019-10-16 14:18:20 +0200

    url manchmal klein

--- Gitlab MR templates.user.js
+++ Gitlab MR templates.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         MR Templates Gitlab
 // @namespace    http://quodata.de/
-// @version      1.2
+// @version      1.3
 // @description  Show merge request templates
 // @author       hannsen
 // @match        https://git04.quodata.de/it/delphi/merge_requests/new*
 // @match        https://git04.quodata.de/it/oequasta/merge_requests/new*
 // @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/merge_requests/new*
+// @match        https://git04.quodata.de/it/oequasta-kollektivbildung/merge_requests/new*
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/merge_requests/new*
 // @match        https://git04.quodata.de/qd_drupal_projects/hub/merge_requests/new*
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_base_entity/merge_requests/new*
@@ -14,14 +15,27 @@
 // @match        https://git04.quodata.de/qd_drupal_projects/qd_datatables_form/merge_requests/new*
 // @downloadURL  https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
 // @updateURL    https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
+// @grant GM_setValue
+// @grant GM_getValue
 // ==/UserScript==
 (function() {
     'use strict';
 
+    var my_template = `### Suggested CHANGELOG entry
+
+### QD-interne Beschreibung & Motivation
+
+### vorher/nachher zu mergende MRs:
+
+### Automatische Tests
+`;
+
+
     $.ajax({
         url: "https://git04.quodata.de/qd_drupal_projects/hub/templates/merge_request/default_template",
     }).done(function(data){
-        $('#merge_request_description').val(data.content);
+        var to_insert = GM_getValue('short_template') ? my_template : data.content;
+        $('#merge_request_description').val(to_insert);
 
         // So the scrollbar shows
         var press = $.Event("keypress");
