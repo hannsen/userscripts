fd08be8356771496aa72ebf2cb9e5e897ba14866
hannsen <jan@hro-home.de>
2020-03-16 16:42:55 +0100

    Update psa_bypass.user.js

--- psa_bypass.user.js
+++ psa_bypass.user.js
@@ -1,40 +1,46 @@
 // ==UserScript==
 // @name         PSA*RIPS Bypasser
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Bypass url shorteners/ads
 // @author       You
 // @match        https://psarips.one/*
-// @match        https://reqbin.com/curl*
-// @grant		 GM_xmlhttpRequest
+// @match        https://psarips.xyz/*
+// @match        https://helloacm.com/curl/*
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var $ = jQuery;
-
     // PSA SIDE
-    $('a[href*="/exit/"]').each(function(){
-        var url = $(this).attr('href');
-        var button = $('<a target="_blank">  Bypass</a>');
-        button.click(function(){
-            GM_xmlhttpRequest({
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                url: "http://X.X.X.X:5000/",
-                data: JSON.stringify({link:url}) ,
-                onload: function(response) {
-                    window.open(response.responseText);
-                }
-            });
-        });
-        $(this).after(button);
+    var dl_links = document.querySelectorAll('a[href*="/exit/"]');
+    dl_links.forEach(function(item){
+        var button = document.createElement('a');
+        button.innerHTML = "  Bypass";
+        button.target = "_blank";
+        button.href ='https://helloacm.com/curl/?url=' + encodeURIComponent(item.href);
+        item.parentNode.insertBefore(button, item.nextSibling);
     });
 
+    // CURL SIDE
+    var config = {attributes: true, childList: true, subtree: true};
+    function waitForSelector (selector, callback) {
+        var waitForLoaded = new MutationObserver(function (mutations) {
+            var elem = document.querySelector(selector);
+            if (elem) {
+                waitForLoaded.disconnect();
+                callback(elem);
+            }
+        });
+        waitForLoaded.observe(document.body, config)
+    }
 
-
+    if(window.location.href.indexOf("helloacm.com") > 0){
+        var elem = document.querySelector('fieldset pre');
+        waitForSelector('fieldset pre', function(elem){
+            var base64url = elem.innerText.match(/&url=(.*)/)[1];
+            location.href = atob(base64url);
+        });
+    }
 
 })();
