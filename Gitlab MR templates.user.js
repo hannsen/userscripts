21f32f09fec1de777616507c66b632d29ce7a8d3
hannsen <jan@hro-home.de>
2020-03-18 10:01:35 +0100

    Update Gitlab MR templates.user.js

--- Gitlab MR templates.user.js
+++ Gitlab MR templates.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         MR Templates Gitlab
 // @namespace    http://quodata.de/
-// @version      1.5
+// @version      1.6
 // @description  Show merge request templates
 // @author       hannsen
-// @match        https://git04.quodata.de/it/delphi/merge_requests/new*
-// @match        https://git04.quodata.de/it/oequasta/merge_requests/new*
-// @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/merge_requests/new*
-// @match        https://git04.quodata.de/it/oequasta-kollektivbildung/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/hub/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_base_entity/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_screenshottests/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_datatables_form/merge_requests/new*
+// @match        https://git04.quodata.de/it/delphi/-/merge_requests/new*
+// @match        https://git04.quodata.de/it/oequasta/-/merge_requests/new*
+// @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/-/merge_requests/new*
+// @match        https://git04.quodata.de/it/oequasta-kollektivbildung/-/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/-/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/hub/-/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_base_entity/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_screenshottests/merge_requests/new*
+// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_datatables_form/merge_requests/new*
 // @downloadURL  https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
 // @updateURL    https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
 // @grant GM_setValue
@@ -20,7 +20,7 @@
 // ==/UserScript==
 (function() {
     'use strict';
-
+GM_setValue('short_template', 1);
     $.ajax({
         url: "https://git04.quodata.de/qd_drupal_projects/hub/templates/merge_request/default_template",
     }).done(function(data){
