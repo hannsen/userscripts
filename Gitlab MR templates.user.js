a9bea7aecae0da4e6196bd83929a3e1bacc4f553
hannsen <loggjohnable@gmail.com>
2022-09-23 12:28:55 +0200

    Update Gitlab MR templates.user.js

--- Gitlab MR templates.user.js
+++ Gitlab MR templates.user.js
@@ -1,41 +1,41 @@
 // ==UserScript==
 // @name         MR Templates GitLab
 // @namespace    http://quodata.de/
-// @version      1.6
+// @version      2.0
 // @description  Show merge request templates
 // @author       hannsen
-// @match        https://git04.quodata.de/it/delphi/-/merge_requests/new*
-// @match        https://git04.quodata.de/it/oequasta/-/merge_requests/new*
-// @match        https://git04.quodata.de/it/OEQUASTA-Kollektivbildung/-/merge_requests/new*
-// @match        https://git04.quodata.de/it/oequasta-kollektivbildung/-/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/qd_ptop_modules_project/-/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/hub/-/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_base_entity/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_screenshottests/merge_requests/new*
-// @match        https://git04.quodata.de/qd_drupal_projects/-/qd_datatables_form/merge_requests/new*
-// @downloadURL  https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
-// @updateURL    https://github.com/hannsen/userscripts/raw/master/Gitlab%20MR%20templates.user.js
+// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @match        https://git04.quodata.de/*/*/-/merge_requests/new*
+// @downloadURL  https://git04.quodata.de/quodata/userscripts/-/raw/master/Gitlab%20MR%20templates.user.js
+// @updateURL    https://git04.quodata.de/quodata/userscripts/-/raw/master/Gitlab%20MR%20templates.user.js
 // @grant GM_setValue
 // @grant GM_getValue
 // ==/UserScript==
 (function() {
     'use strict';
 
-    $.ajax({
+
+
+
+    function startRequest(){
+        jQuery.ajax({
         url: "https://git04.quodata.de/qd_drupal_projects/hub/templates/merge_request/default_template",
     }).done(function(data){
         var to_insert = data.content;
         if(GM_getValue('short_template')){
             to_insert = filterToShortTemplate(to_insert);
         }
-        $('#merge_request_description').val(to_insert);
+        jQuery('#merge_request_description').val(to_insert);
 
         // So the scrollbar shows
-        var press = $.Event("keypress");
+        var press = jQuery.Event("keypress");
         press.ctrlKey = false;
         press.which = 32; //Space
-        $("#merge_request_description").trigger(press);
+        jQuery("#merge_request_description").trigger(press);
     });
+    }
+
+    setTimeout(startRequest, 100);
 
     function filterToShortTemplate(input){
         var lines = input.split('\n');
@@ -49,3 +49,4 @@
 
 
 })();
+
