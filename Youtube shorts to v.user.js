a3c11c7beb9ca38739bb6e842487dfcef3123b78
hannsen <loggjohnable@gmail.com>
2022-09-24 21:52:24 +0200

    Update Youtube shorts to v.user.js

--- Youtube shorts to v.user.js
+++ Youtube shorts to v.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Redirect to video view of shorts
 // @author       You
-// @match        https://www.youtube.com/shorts/*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -12,5 +12,18 @@
 (function() {
     'use strict';
 
-   window.location.href =   window.location.href.replace('shorts', 'v')
+    function checkAndRedir(){
+        if(window.location.href.startsWith("https://www.youtube.com/shorts/")){
+            window.location.href = window.location.href.replace('shorts', 'v')
+        }
+    }
+    checkAndRedir();
+
+
+    var config = {attributes: false, childList: true, subtree: false};
+    var waitForLoaded = new MutationObserver(function (mutations) {
+        checkAndRedir();
+    });
+    waitForLoaded.observe(document.body, config)
+
 })();
