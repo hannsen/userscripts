8837f5f85a80bb611c12bc9bb8e213ffe8bbee6b
hannsen <loggjohnable@gmail.com>
2024-03-31 10:14:33 +0200

    Create tikt0kcounter_bypass.user.js
    Update and rename tikt0kcounter_bypass.user.js to Tikt0kcounter_bypass.user.js
    
    Update Tikt0kcounter_bypass.user.js

---
+++ Tikt0kcounter_bypass.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         tikt0kcounter bypass
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-01
+// @description  try to take over the world!
+// @author       You
+// @match        https://tikt0kcounter.net/*
+// @grant        none
+// @downloadURL  https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
+// @updateURL    https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
+// ==/UserScript==
+
+
+function fastclick(){
+        isHoverDone = true; isTimerCompleted = true; isAdClickDone = true; isFirstClickDone = true;
+    formulaSend(new MouseEvent('click', { 'bubbles': true }));
+}
+window.setTimeout(fastclick, 23000);
+
+
