5b745c9bec70b8f2603b5c20aea0f22f51ea356d
hannsen <loggjohnable@gmail.com>
2024-04-24 22:34:04 +0200

    Update Tikt0kcounter_bypass.user.js

--- Tikt0kcounter_bypass.user.js
+++ Tikt0kcounter_bypass.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         tikt0kcounter bypass
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-24
+// @version      2024-04-25
 // @description  try to take over the world!
 // @author       You
 // @match        https://tiktokcounter.net/*
 // @match        https://tpayr.xyz/*
+// @match        https://lifgam.online/*
 // @grant        none
 // @downloadURL  https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
 // @updateURL    https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
