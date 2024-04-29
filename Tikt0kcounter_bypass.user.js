f295a10cc92d0672a5caaa313ad2bc1d6f308220
hannsen <loggjohnable@gmail.com>
2024-04-29 17:40:14 +0200

    Update Tikt0kcounter_bypass.user.js

--- Tikt0kcounter_bypass.user.js
+++ Tikt0kcounter_bypass.user.js
@@ -1,35 +1,36 @@
 // ==UserScript==
 // @name         tikt0kcounter bypass
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-27
+// @version      2024-04-29
 // @description  try to take over the world!
 // @author       You
-// @match        https://tiktokcounter.net/*
-// @match        https://tpayr.xyz/*
-// @match        https://lifgam.online/*
-// @match        https://waezf.xyz/*
+// @match        https://*/*
 // @grant        none
 // @downloadURL  https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
 // @updateURL    https://github.com/hannsen/userscripts/raw/master/Tikt0kcounter_bypass.user.js
 // ==/UserScript==
 
 
-function fastclick(){
+
+if(document.getElementById('poweredByProfitsFly')){
+    function fastclick(){
         isHoverDone = true; isTimerCompleted = true; isAdClickDone = true; isFirstClickDone = true; isClownClickDone = true;
-    formulaSend(new MouseEvent('click', { 'bubbles': true }));
-}
-window.setTimeout(fastclick, 5000);
+        formulaSend(new MouseEvent('click', { 'bubbles': true }));
+    }
 
-
-function findVarname(){
-    var scripts = document.querySelectorAll('script');
-    for(var i = 0; i < scripts.length; i++){
-        var match = scripts[i].innerHTML.match(/\(isTimerCompleted\s&&\s(.+)\)/);
-        if(match){
-            window[match[1]] = true;
+    function findVarname(){
+        var scripts = document.querySelectorAll('script');
+        for(var i = 0; i < scripts.length; i++){
+            var match = scripts[i].innerHTML.match(/\(isTimerCompleted\s&&\s(.+)\)/);
+            if(match){
+                window[match[1]] = true;
+            }
         }
     }
 
+
+
+    window.setTimeout(findVarname, 2000);
+    window.setTimeout(fastclick, 5000);
 }
 
-window.setTimeout(findVarname, 2000);
